de29d833683302d467705b49dd767cdc4a4d318b
evgenbl47 <evgenbl47@gmali.com>
2025-02-17 20:32:39 +0500

    add some comments

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!\n");
-
         ProductCatalog catalog = new ProductCatalog();
 
         Product shirt = new Product("Shirt", 1500.0, 4.8);
@@ -33,14 +27,17 @@ public class Main {
             cart.addProduct(foundSocks);
         }
 
-
         cart.showCart();
-
         cart.removeProduct("Socks");
-
         cart.showCart();
 
         Order order = new Order(cart, "Tom Smith", "US, New-York");
         order.displayOrderDetails();
     }
 }
+
+/*
+    Класс Product: Определяет товары.
+    Класс Cart: Управляет корзиной покупок, позволяя добавлять и удалять товары.
+    Класс ProductCatalog: Хранит ассортимент товаров и дает возможность их добавлять, удалять и искать.
+ */
