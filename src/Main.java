9ea09a610d5185ef92a5fcbe58255377427d717a
evgenbl47 <evgenbl47@gmali.com>
2025-02-17 16:43:54 +0500

    add new line

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!\n");
+        System.out.println("\n");
 
         Product shirt = new Product("Shirt", 1500.0, 4.8);
         Product jeans = new Product("Jeans", 3500.0, 4.5);
